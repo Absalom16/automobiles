d8b1c59b2e9ebfecca868d0c188611efdc15e805
Absalom16 <118267058+Absalom16@users.noreply.github.com>
2023-01-30 11:27:00 +0300

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <header class="container-fluid" id="top">
+        <section class="bg-primary">
+            <div class="top container">
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                        <p class="text-light"><b>Email:Maxwheels@gmail.com</b></p>
+                    </div>
+                    <div class="col-12 col-lg-6 text-lg-end">
+                        <p class="text-light"><b>Phone:+25412345678</b></p>
+                    </div>
+                </div>
+                </div>
+        </section>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container">
+
+
+              <a class="navbar-brand" href="#">
+                  <img src="" alt="">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link px-3 active" aria-current="page" href="C:/xampp/htdocs/company website/index.html">Home</a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a class="nav-link  px-3" href="C:/xampp/htdocs/company website/About.html">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  px-3" href="C:/xampp/htdocs/company website/Contacts.html">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  px-3" href="C:/xampp/htdocs/company website/Our Team.html">Our team</a>
+                  </li>
+                  
+                  
+                  <li class="nav-item dropdown">
+                    <a class="nav-link  px-3" href="C:/xampp/htdocs/company website/Services.html">
+                      Services
+                    </a>
+                   
+                  </li>
+                  
+                </ul>
+                <div class="d-flex">
+                  <a href="#"><img src="" alt="" >facebook</a>&nbsp;&nbsp;
+                  <a href="#"><img src="" alt="">twitter</a>&nbsp;&nbsp;
+                  <a href="#"><img src="" alt="">instagram</a>&nbsp;&nbsp;
+                  <a href="#"><img src="" alt="">linkedin</a>&nbsp;&nbsp;
+                  <a href="#"><img src="" alt="">whatsapp</a>
+                </div>
+              </div>
+            </div>
+          </nav>
+         
+
+    </header>
+
+
+
+
+
+
+
+<footer class="container-fluid bg-dark py-5">
+<div class="row">
+    <div class="col-12 text-center">
+        <a href="# top"><img src="" alt="">icon</a>
+    </div>
+  <div class="col-12 col-lg-2 offset-lg-5 mt-3">
+      <form action="">
+        <div class="input-group mb-4">
+            <input required type="text" class="form-control" placeholder="Your email" aria-label="Your email" aria-describedby="button-addon2">
+            <button class="btn btn-primary" type="button" id="button-addon2">Subscribe</button>
+          </div>
+      </form>
+  </div>
+  <div class="col-12 text-center pt-4">
+    <a href="#"><img src="" alt="" >facebook</a>&nbsp;&nbsp;
+    <a href="#"><img src="" alt="">twitter</a>&nbsp;&nbsp;
+    <a href="#"><img src="" alt="">instagram</a>&nbsp;&nbsp;
+    <a href="#"><img src="" alt="">linkedin</a>&nbsp;&nbsp;
+    <a href="#"><img src="" alt="">whatsapp</a>
+  </div>  
+  <div class="col-12 text-center mt-4">
+      <p class="text-secondary">CopyRight &copy; Maxwheels</p>
+  </div>
+</div>
+</footer>
+
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
